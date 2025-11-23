1307cc23ad9f68f6c843b5195ebde0ce8ada7173
Nikunj-Mehta <mehtanikunj11@gmail.com>
2025-11-23 21:18:38 +0530

    Completed Sidebar component with components and styling similar to ChatGPT

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -2,9 +2,25 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div>
-      Sidebar
-    </div>
+    <section className="sidebar">
+      {/* new chat button  */}
+      <button>
+        <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo"></img>
+        <span><i className="fa-regular fa-pen-to-square"></i></span>
+      </button>
+
+      {/* history */}
+      <ul className="history">
+        <li>Thread 1</li>
+        <li>Thread 2</li>
+        <li>Thread 3</li>
+      </ul>
+
+      {/* sign */}
+      <div className="sign">
+        <p>By Nikunj Mehta 🧑‍💻</p>
+      </div>
+    </section>
   )
 }
 
